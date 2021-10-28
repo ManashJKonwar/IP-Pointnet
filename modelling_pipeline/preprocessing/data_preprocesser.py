9fddf49fc8a355c0c6b0ef596110a38c3efb551a
ManashJKonwar <rickykonwar@gmail.com>
2021-10-28 22:48:38 +0530

    Enhancement: Adding_data_preprocessor_script
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

---
+++ modelling_pipeline/preprocessing/data_preprocesser.py
@@ -0,0 +1,53 @@
+__author__ = "konwar.m"
+__copyright__ = "Copyright 2021, AI R&D"
+__credits__ = ["konwar.m"]
+__license__ = "Individual Ownership"
+__version__ = "1.0.1"
+__maintainer__ = "konwar.m"
+__email__ = "rickykonwar@gmail.com"
+__status__ = "Development"
+
+import os, glob
+import trimesh
+import numpy as np
+
+def parse_dataset(dataset_directory=None, num_points=2048):
+    """
+    Read all meshed off files per 10 classes and return the numpy arrays for the same
+    Parameters: 
+        dataset_directory (str): http link for downloading point cloud dataset
+        num_points (int): number of points by which each mesh file needs to be generated for
+                        converting it into point mesh data
+    Returns: 
+        tuple of numpy arrays representating all files under train and test folders for each classes
+    """
+    train_points = []
+    train_labels = []
+    test_points = []
+    test_labels = []
+    class_map = {}
+    folders = glob.glob(os.path.join(dataset_directory, "[!README]*"))
+
+    for i, folder in enumerate(folders):
+        print("processing class: {}".format(os.path.basename(folder)))
+        # store folder name with ID so we can retrieve later
+        class_map[i] = folder.split("/")[-1]
+        # gather all files
+        train_files = glob.glob(os.path.join(folder, "train/*"))
+        test_files = glob.glob(os.path.join(folder, "test/*"))
+
+        for f in train_files:
+            train_points.append(trimesh.load(f).sample(num_points))
+            train_labels.append(i)
+
+        for f in test_files:
+            test_points.append(trimesh.load(f).sample(num_points))
+            test_labels.append(i)
+
+    return (
+        np.array(train_points),
+        np.array(test_points),
+        np.array(train_labels),
+        np.array(test_labels),
+        class_map,
+    )
