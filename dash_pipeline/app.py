aa3d2cb888352ff31fa764a2be48683aa69ceb90
ManashJKonwar <rickykonwar@gmail.com>
2021-11-05 13:21:36 +0530

    Enhancement: Setting_up_dash_app
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

---
+++ dash_pipeline/app.py
@@ -0,0 +1,19 @@
+__author__ = "konwar.m"
+__copyright__ = "Copyright 2021, AI R&D"
+__credits__ = ["konwar.m"]
+__license__ = "Individual Ownership"
+__version__ = "1.0.1"
+__maintainer__ = "konwar.m"
+__email__ = "rickykonwar@gmail.com"
+__status__ = "Development"
+
+import dash
+import logging, logging.config
+import dash_bootstrap_components as dbc
+from flask import Flask
+
+FA = "https://use.fontawesome.com/releases/v5.15.1/css/all.css"
+external_stylesheets = [dbc.themes.BOOTSTRAP, FA]
+server = Flask(__name__) 
+app = dash.Dash(external_stylesheets=external_stylesheets, server=server)
+app.config.suppress_callback_exceptions = True
